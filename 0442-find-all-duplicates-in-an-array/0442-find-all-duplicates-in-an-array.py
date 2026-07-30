ed9dba8bcafe7d66c7094cc99aa604711b0bcf1b
priyanshi220383-max <priyanshi220383@gmail.com>
2026-07-30 23:24:58 +0530

    Time: 28 ms (91.3%), Space: 24.8 MB (30.6%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def findDuplicates(self, nums):
-        seen = set()
-        ans = []
+        seen = set() #empty set created that stores no already seen
+        ans = [] #empty list in which duplicate or single accuring item is added
 
         for num in nums :
 
